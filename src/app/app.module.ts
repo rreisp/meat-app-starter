1009a7cdf7c206e7824d6184c9e7db15447f88f2
Rodrigo Reis <rreisp@gmail.com>
2019-07-31 15:58:36 -0300

    Criando as Primeiras Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
+import { ROUTES } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
