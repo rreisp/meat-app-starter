a63ffb9789c1c619216c73baa7f015094d66b8f0
Rodrigo Reis <rreisp@gmail.com>
2019-08-09 13:25:46 -0300

    Implementando os Itens de Menu - Pipe Currency

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -1,14 +1,25 @@
+import { MenuItem } from './../menu-item/menu-item.model';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mt-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
+  menu: Observable<MenuItem[]>;
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.menu = this.restaurantService.menuOfRestaurant(this.route.parent.snapshot.params.id);
+    // this.menu = this.restaurantService.menuOfRestaurant(this.route.parent.snapshot.params['id']);
   }
 
+  addMenuItem(item: MenuItem) {
+    console.log(item);
+  }
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,3 +1,4 @@
+import { MenuItem } from './../restaurant-detail/menu-item/menu-item.model';
 import { ErrorHandler } from './../app.error-handler';
 import { MEAT_API } from './../app.api';
 import { Injectable } from '@angular/core';
@@ -29,6 +30,20 @@ export class RestaurantsService {
                                 catchError(ErrorHandler.handleError)
                         );
                 // return  this.http.get(`${MEAT_API}/restaurants/${id}`)
+                // .map(response => response.json())
+                // .catch(ErrorHandler.handleError)
+        }
+
+        reviewsOfRestaurant(id: string): Observable<any> {
+                return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`);
+        }
+
+        menuOfRestaurant(id: string): Observable<MenuItem[]> {
+                return this.http.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`)
+                        .pipe(
+                                catchError(ErrorHandler.handleError)
+                        );
+                // return  this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
                 // .map(response => response.json())
                 // .catch(ErrorHandler.handleError)
         }
