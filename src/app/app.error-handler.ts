9d4591f30c6d08507976c4622ba2c9a7447026e8
Rodrigo Reis <rreisp@gmail.com>
2019-08-09 08:54:52 -0300

    Tratamento de Erros com o Operador Catch

---
+++ src/app/app.error-handler.ts
@@ -0,0 +1,17 @@
+// import {Response} from '@angular/http';
+import { Observable } from 'rxjs';
+import { throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export class ErrorHandler {
+        static handleError(error: HttpErrorResponse | any) {
+                let errorMessage: string;
+                if (error instanceof HttpErrorResponse) {
+                        errorMessage = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`;
+                } else {
+                        errorMessage = error.toString();
+                }
+                console.log(  errorMessage  );
+                return throwError(errorMessage);
+        }
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,17 +1,26 @@
+import { ErrorHandler } from './../app.error-handler';
 import { MEAT_API } from './../app.api';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import 'rxjs/add/operator/map';
+import { catchError } from 'rxjs/operators';
 
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantsService {
         constructor(private http: HttpClient) { }
 
         restaurants(): Observable<Restaurant[]> {
-                return  this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`);
-                // return  this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`).map(response => response.json());
+                return  this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`)
+                        .pipe(
+                                catchError( ErrorHandler.handleError )
+                        );
+
+                // return  this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`)
+                // .map(response => response.json())
+                // .catch(ErrorHandler.handleError)
         }
 }
