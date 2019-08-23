e2015b714ba75131d08daef6c61e83445bcede55
Rodrigo Reis <rreisp@gmail.com>
2019-08-23 15:34:40 -0300

    Criando um Core Module Com Serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { ROUTES } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -11,18 +11,15 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-
-import { registerLocaleData } from '@angular/common';
-import ptBr from '@angular/common/locales/pt';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 registerLocaleData(ptBr);
 
@@ -44,9 +41,10 @@ registerLocaleData(ptBr);
     BrowserModule,
     HttpClientModule,
     SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, { provide: LOCALE_ID, useValue: 'pt' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
