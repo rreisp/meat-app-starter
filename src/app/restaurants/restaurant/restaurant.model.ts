482112527e1f38329d27257ebf2b5c116fa41af3
Rodrigo Reis <rreisp@gmail.com>
2019-08-27 09:42:34 -0300

    Construindo a Aplicação

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -1,8 +1,10 @@
-export   interface Restaurant {
+export interface Restaurant {
         id: string;
         name: string;
         category: string;
         deliveryEstimate: string;
         rating: number;
         imagePath: string;
+        hours?: string;
+        about?: string;
 }
