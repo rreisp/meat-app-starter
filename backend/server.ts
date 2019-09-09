cb9f654d6424fb30252676f0d8a86688f7779b9b
Rodrigo Reis <rreisp@gmail.com>
2019-09-09 10:53:10 -0300

    Implementando a Rota de Login

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import { Express } from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
 
+import { handleAuthentication } from './auth';
+
 const server: Express = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -20,7 +22,8 @@ server.get('/echo', (req, res) => {
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 
-
+// middleware para login
+server.post('/login', handleAuthentication);
 
 server.use((req, res, next) => {
         if (req.method === 'POST') {
